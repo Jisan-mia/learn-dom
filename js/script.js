84c297c15e788e20be4e011d8a49bbadbe37f418
Jisan-mia <jisanmia47@gmail.com>
2021-06-09 20:26:18 +0600

    done with the project

--- js/script.js
+++ js/script.js
@@ -16,16 +16,23 @@ let createTodo = function (todo) {
 	const liElem = document.createElement("li");
 	const checkbox = document.createElement("input");
 	const labelElm = document.createElement("label");
+	const editInputElm = document.createElement("input");
 	const editBtnElm = document.createElement("button");
+	const deleteBtnElm = document.createElement("button");
 
 	labelElm.innerText = todo;
 	checkbox.type = "checkbox";
+	editInputElm.type = "text";
 	editBtnElm.innerText = "Edit";
 	editBtnElm.className = "btn edit-btn";
+	deleteBtnElm.innerText = "Delete";
+	deleteBtnElm.className = "btn delete-btn";
 
 	liElem.appendChild(checkbox);
 	liElem.appendChild(labelElm);
+	liElem.appendChild(editInputElm);
 	liElem.appendChild(editBtnElm);
+	liElem.appendChild(deleteBtnElm);
 
 	return liElem;
 };
@@ -33,62 +40,107 @@ let createTodo = function (todo) {
 let addTodo = function (e) {
 	e.preventDefault();
 
-	let liElm = createTodo(todoInput.value);
+	let liElem = createTodo(todoInput.value);
 
-	activeTodoLists.appendChild(liElm);
+	activeTodoLists.appendChild(liElem);
 	todoInput.value = "";
 
 	// bind the new list item to the activeTodo list
-	bindActiveTodoLists(liElm, completeTodo, editTodo);
+	bindActiveTodoLists(liElem, completeTodo, editTodo, deleteTodo);
 };
 
 let completeTodo = function () {
-	let liElm = this.parentNode;
+	let liElem = this.parentNode;
 
-	let deleteBtnElm = document.createElement("button");
-	deleteBtnElm.innerText = "Delete";
-	deleteBtnElm.className = "delete-btn btn";
-
-	liElm.appendChild(deleteBtnElm);
-
-	let checkbox = liElm.querySelector('input[type="checkbox"]');
-	checkbox.remove();
-	let editBtn = liElm.querySelector(".edit-btn");
+	// let checkbox = liElem.querySelector('input[type="checkbox"]');
+	// checkbox.remove();
+	let editBtn = liElem.querySelector(".edit-btn");
 	editBtn.remove();
 
-	completeTodoLists.appendChild(liElm);
+	completeTodoLists.appendChild(liElem);
 
-	bindCompleteTodoLists(liElm, deleteTodo);
+	bindCompleteTodoLists(liElem, deleteTodo, inCompleteTodo);
+};
+
+let inCompleteTodo = function () {
+	let liElem = this.parentNode;
+	let deleteBtn = liElem.querySelector(".delete-btn");
+	let editBtn = document.createElement("button");
+	editBtn.innerText = "Edit";
+	editBtn.className = "btn edit-btn";
+	liElem.insertBefore(editBtn, deleteBtn);
+
+	activeTodoLists.appendChild(liElem);
+	bindActiveTodoLists(liElem, completeTodo, editTodo, deleteTodo);
 };
 
 let editTodo = function () {
-	alert("Edit functionality is one way...");
+	let liElem = this.parentNode;
+
+	let editInput = liElem.querySelector("input[type=text]");
+	let label = liElem.querySelector("label");
+	let containsClass = liElem.classList.contains("editMode");
+
+	//If class of the parent is .editmode
+	if (containsClass) {
+		//switch to .editmode
+		//label becomes the inputs value.
+		label.innerText = editInput.value;
+	} else {
+		editInput.value = label.innerText;
+	}
+
+	//toggle .editmode on the parent.
+	liElem.classList.toggle("editMode");
 };
 
 let deleteTodo = function () {
-	let liElm = this.parentNode;
-	let ul = liElm.parentNode;
-	ul.removeChild(liElm);
+	let liElem = this.parentNode;
+	let ul = liElem.parentNode;
+	ul.removeChild(liElem);
 };
 
-let bindActiveTodoLists = function (todoItem, checkboxClick, editBtnClick) {
+let bindActiveTodoLists = function (
+	todoItem,
+	checkboxClick,
+	editBtnClick,
+	deleteClick
+) {
 	let checkbox = todoItem.querySelector('input[type="checkbox"]');
 	checkbox.onchange = checkboxClick;
 	let editBtn = todoItem.querySelector(".edit-btn");
 	editBtn.onclick = editBtnClick;
+	let deleteBtn = todoItem.querySelector(".delete-btn");
+	deleteBtn.onclick = deleteClick;
 };
 
-let bindCompleteTodoLists = function (todoItem, deleteButtonClick) {
+let bindCompleteTodoLists = function (
+	todoItem,
+	deleteButtonClick,
+	unCheckClick
+) {
 	let deleteBtn = todoItem.querySelector(".delete-btn");
 	deleteBtn.onclick = deleteButtonClick;
+
+	let checkbox = todoItem.querySelector("input[type=checkbox]");
+	checkbox.onchange = unCheckClick;
 };
 
 for (let i = 0; i < activeTodoLists.children.length; i++) {
-	bindActiveTodoLists(activeTodoLists.children[i], completeTodo, editTodo);
+	bindActiveTodoLists(
+		activeTodoLists.children[i],
+		completeTodo,
+		editTodo,
+		deleteTodo
+	);
 }
 
 for (let i = 0; i < completeTodoLists.children.length; i++) {
-	bindCompleteTodoLists(completeTodoLists.children[i], deleteTodo);
+	bindCompleteTodoLists(
+		completeTodoLists.children[i],
+		deleteTodo,
+		inCompleteTodo
+	);
 }
 
 submitForm.addEventListener("submit", addTodo);
