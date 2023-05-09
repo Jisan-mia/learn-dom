874524c9d297a2d08e3b3aaba51cfcaef36755f6
Jisan-mia <jisanmia47@gmail.com>
2023-05-09 16:49:26 +0600

    private properties inside class

--- projects/oop-calculator/practice.class.js
+++ projects/oop-calculator/practice.class.js
@@ -63,9 +63,10 @@ class Student extends GenericPersonClass {
 }
 
 class Employer extends GenericPersonClass {
-    constructor(name, age, position) {
+    constructor(name, age, position, salary) {
         super(name, age)
-        this.position = position
+        this.position = position;
+        this.salary = salary
     }
 
     getEntitle() {
@@ -76,7 +77,37 @@ class Employer extends GenericPersonClass {
 const jisanStudent = new Student('Jisan', 21, 2341)
 console.log(jisanStudent.getEntitle())
 
-const employerJack = new Employer('Jack', 32, 'HR Mangement')
+const employerJack = new Employer('Jack', 32, 'HR Mangement', 20000)
 console.log(employerJack.position)
 
+console.log('======== private properties/methods in class ======')
+// private properties/methods inside of a class is used to create private props/methods that are only accessible inside the class and not accessible outside of the class 
+class StudentClassWithPrivateProperties extends Student {
+    constructor(name, age, studentId) {
+        super(name, age, studentId)
+    }
 
+    // private property
+    #isMarried = true;
+
+    /**
+     * @param {boolean} value
+     */
+    set isMarried(value) {
+        return this.#isMarried = value
+    }
+
+    getStudentMaritalStatus() {
+        if(this.#isMarried) {
+            console.log(this.#isMarried + "... its a joke!, i'm not married")
+        } else {
+            console.log(this.#isMarried+ ', obviously not')
+        }
+    }
+
+    
+}
+
+const studentWithPrivateProp = new StudentClassWithPrivateProperties('Shawon', 21, 3523)
+studentWithPrivateProp.isMarried = false
+console.log(studentWithPrivateProp.getStudentMaritalStatus())
